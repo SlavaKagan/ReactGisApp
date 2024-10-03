b7bc024e57535bd1c793dc7cce46c701ccff09bf
SK <slava15.92@gmail.com>
2024-10-03 19:05:18 +0300

    Change files

--- GisReactProject/src/App.jsx
+++ GisReactProject/src/App.jsx
@@ -1,18 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import MapView from '@arcgis/core/views/MapView';
+import WebMap from '@arcgis/core/WebMap';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const webMap = new WebMap({
+      basemap: 'streets-navigation-vector',
+    });
+
+    const view = new MapView({
+      container: mapRef.current, 
+      map: webMap,
+      center: [34.8516, 31.0461], // Israel coordinates
+      zoom: 5,
+    });
+
+    const capitalCitiesLayer = new FeatureLayer({
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Cities/FeatureServer/0',
+      definitionExpression: "STATUS = 'National and provincial capital'",
+      outFields: ['CITY_NAME', 'POP'],
+      popupTemplate: {
+        title: '{CITY_NAME}',
+        content: 'Population: {POP}',
+      },
+      renderer: {
+        type: 'simple',
+        symbol: {
+          type: 'simple-marker',
+          color: 'red',
+          size: '10px',
+        },
+      },
+    });
+
+    webMap.add(capitalCitiesLayer);
+
+    return () => {
+      if (view) {
+        view.destroy();
+      }
+    };
+  }, []);
 
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <div>
+      <h1>GIS React Application</h1>
+      <div id="mapContainer" ref={mapRef} style={{ height: '600px', width: '100%' }}></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ReactGisApp
+
+## Contact
+**Email:** slava.kagan.ht@gmail.com
+
+## How to use this service
+
+Clone the repository to your local repository folder: https://github.com/SlavaKagan/ReactGisApp <br />
+Open the repository with Terminal or other IDE. <br />
+
+1. cd /GisReactProject <br />
+2. npm install <br />
+3. npm run dev <br />
+
+## General info about the task
+**GitHub repository:** https://github.com/SlavaKagan/ReactGisApp <br />
+
+## Frontend
+**Programming Language:** (Javascript) ES6 version https://www.javascript.com/ <br />
+**Framework:** <br /> 
+React 18 version https://react.dev/ <br />
+Vite 5 version https://vite.dev/guide/ <br />
+
+## Abstract
+
+React-based GIS web-application using ArcGIS Maps SDK for JavaScript. <br />
+The application display a map zoomed on Israel. <br />
+The app displays capital cities from all over the world. <br />
+By clicking on the point of a city (should be red symbol) a popup will display the name of the city and the population only. <br />
+Example below: <br />
+https://www.arcgis.com/home/item.html?id=d9677f2ef1d547c29fc30e628596f0c0
